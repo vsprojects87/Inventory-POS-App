0666fa6a859fdb1052b4662ee2c36d6b89cd8a07
vsprojects87 <vs.scala.619@gmail.com>
2024-05-19 12:53:00 +0530

    Fixed Isssues

--- InventoryPOS/Data/ApplicationDBContext.cs
+++ InventoryPOS/Data/ApplicationDBContext.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using InventoryPOS.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace InventoryPOS.Data
 {
-    public class ApplicationDBContext :IdentityDbContext<IdentityUser>
+    public class ApplicationDBContext :IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options)
         {
         }
-
-    }
+    
+	}
 }
